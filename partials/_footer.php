4eba7a5dd304ce2def5c64cb964bebd85bc2a808
imtiaza1 <emtiazaleeee@gmail.com>
2024-04-05 11:20:00 +0500

    update

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,6 +1,6 @@
-<footer class=" container-fluid text-center text-lg-start" style="background-color: black; top: 32px;
-    position: relative;">
+<footer class="container-fluid text-center text-lg-start" style="background-color: black; position: relative; top: 32px;">
     <div class="container d-flex justify-content-center py-5">
+        <!-- Social Media Buttons -->
         <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
             <i class="fab fa-facebook-f"></i>
         </button>
@@ -16,15 +16,15 @@
     </div>
 
     <!-- Copyright -->
-    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2); width:100%;">
+    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2); width: 100%;">
         © 2024 Copyright:
-        <a class="text-white" href="https://mdbootstrap.com/">IDisscuss.com</a>
+        <a class="text-white text-decoration-none" href="https://mdbootstrap.com/" target="_blank">IDiscuss.com</a>
     </div>
-    <!-- Copyright -->
 </footer>
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
+    crossorigin="anonymous"></script>
 </body>
-
 </html>
